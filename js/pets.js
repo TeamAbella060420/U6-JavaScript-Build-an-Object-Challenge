b8f89e3bd3367d939c9e7ace6808fbc53d9e8a28
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-09 12:06:54 +0300

    Update pets.js

--- js/pets.js
+++ js/pets.js
@@ -3,3 +3,45 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
+
+const pets = [
+  {
+    name: 'Ben',
+    type: 'Dog',
+    breed: 'Australian Shepherd',
+    age: 12,
+    photo: 'img/aussie.jpg'
+  },
+  {
+    name: 'Noel',
+    type: 'Dog',
+    breed: 'Shiwawa',
+    age: 8,
+    photo: 'img/dachshund.jpg'
+  },
+  {
+    name: 'Carmello',
+    type: 'Dog',
+    breed: 'Brownie',
+    age: 13,
+    photo: 'img/golden.jpg'
+  },  {
+    name: 'Ricky',
+    type: 'Cat',
+    breed: 'Clingy',
+    age: 10,
+    photo: 'img/persian.jpg'
+  },  {
+    name: 'Martin',
+    type: 'Dog',
+    breed: 'Pug',
+    age: 10,
+    photo: 'img/pug.jpg'
+  },  {
+    name: 'Faustino',
+    type: 'Cat',
+    breed: 'tabby',
+    age: 9,
+    photo: 'img/tabby.jpg'
+  },
+];
